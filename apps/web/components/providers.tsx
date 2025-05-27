8ed015999734c2479b590d8256ae89c8b49ed20c
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2025-05-27 22:33:25 +0200

    add next blog to bundled css

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { cn } from '@workspace/ui/lib/utils';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
